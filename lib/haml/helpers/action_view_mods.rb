01902ca30cda4ee78ae85d324cc27a30d1a38bae
Daniel Neighman <has.sox@gmail.com>
2010-04-10 21:04:28 +1000

    Removes an issue where the helpers infinitely recurse when using edge rails3 and datamapper

--- lib/haml/helpers/action_view_mods.rb
+++ lib/haml/helpers/action_view_mods.rb
@@ -125,10 +125,15 @@ module ActionView
         @template_object.send :is_haml?
       end
 
-      unless defined?(ActionView::Helpers) && defined?(ActionView::Helpers::ActiveRecordInstanceTag)
-        alias_method :content_tag_without_haml, :content_tag
-        alias_method :content_tag, :content_tag_with_haml
+      def content_tag(*args)
+        content_tag_with_haml *args
       end
+#      unless defined?(ActionView::Helpers) && defined?(ActionView::Helpers::ActiveRecordInstanceTag)
+#        unless method_defined?(:content_tag_without_haml)
+#          alias_method :content_tag_without_haml, :content_tag
+#          alias_method :content_tag, :content_tag_with_haml
+#        end
+#      end
     end
 
     if Haml::Util.ap_geq_3?
